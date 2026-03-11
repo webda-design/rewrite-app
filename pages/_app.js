6d4afcf2a840fa3afa8a4cc1d773a51df60ad1eb
webda0917 <31466336+webda0917@users.noreply.github.com>
2026-03-11 09:52:10 +0900

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,14 @@
+import Head from "next/head";
 import "../styles/globals.css";
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="robots" content="noindex, nofollow" />
+        <title>リライトAPP | ストレンジブレイン</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
